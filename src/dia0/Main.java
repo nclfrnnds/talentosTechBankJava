3cc41ba6dc5a66228b05de808b37b8bf79263529
Nicoli <nicoli1992@gmail.com>
2021-06-28 22:00:27 -0300

    primeiro commit do famoso hello world

---
+++ src/dia0/Main.java
@@ -0,0 +1,10 @@
+package dia0;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+        System.out.println(System.getProperty("java.version"));
+    }
+
+}
